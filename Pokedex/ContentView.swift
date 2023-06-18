95918e7cf143d8b995d476a5cea68acd3bc4ca9a
Mike Paraskevopoulos <mikeparaskevopoulos.mp@gmail.com>
2023-06-18 04:41:25 +0300

    Initial Commit

---
+++ Pokedex/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Pokedex
+//
+//  Created by Mike Paraskevopoulos on 18/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Mike Paraskevopoulos on 18/6/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
