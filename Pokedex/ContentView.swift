9fe3bf70d81ad36385482693354bd4fe0ac73bec
Mike Paraskevopoulos <mikeparaskevopoulos.mp@gmail.com>
2023-06-21 04:16:48 +0300

    shiny mode

--- Pokedex/ContentView.swift
+++ Pokedex/ContentView.swift
@@ -10,12 +10,13 @@ import Kingfisher
 
 struct ContentView: View {
     @ObservedObject var apiManager = ApiManager()
-    @State private var selectedPokemonIndex = 448
+    @State private var selectedPokemonIndex = 1
     @State var loading = true
     @State private var isAnimating = false
     @State private var showProgress = false
     @State private var searchText = ""
     @State var isImageFront=true
+    @State var isShiny=false
     
     var foreverAnimation: Animation {
         Animation.linear(duration: 2.0)
@@ -28,12 +29,12 @@ struct ContentView: View {
     
     @State var nameIdMap=[String:Int]()
     @State private var animateFlag = true
-
+    
     func fetchPokemon() async {
         await apiManager.fetchPokemon(from: 0, to: 1010)
-//        for pokemon in apiManager.pokemonList{
-//            nameIdMap[pokemon.name.capitalized] = pokemon.id
-//        }
+        //        for pokemon in apiManager.pokemonList{
+        //            nameIdMap[pokemon.name.capitalized] = pokemon.id
+        //        }
         await pokemonChanged(selectedPokemonIndex)
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
             loading = false
@@ -63,21 +64,28 @@ struct ContentView: View {
                     .ignoresSafeArea(.all)
                 ZStack{
                     VStack{
-                            HStack{
-                                Image(systemName: "circle.fill").foregroundColor(.green).opacity(self.isAnimating ? 1 : 0)
-                                    .animation(Animation.easeInOut(duration: 2).repeatForever(autoreverses: false),value:isAnimating)
-                                    .onAppear{self.isAnimating = true}
-                                    .onDisappear(){self.isAnimating = false}
-                                Spacer()
-                            }.padding(.leading,20)
                         HStack{
+                            Image(systemName: "circle.fill").foregroundColor(.green).padding(.bottom,10).opacity(self.isAnimating ? 1 : 0).animation(Animation.easeInOut(duration: 2).repeatForever(autoreverses: false),value:isAnimating)
+                                .onAppear{self.isAnimating = true}
+                                .onDisappear(){self.isAnimating = false}
                             Spacer()
+                        }.padding(.leading,20)
+                        HStack{
                             if animateFlag{
+                                Button {
+                                    isShiny.toggle()
+                                } label: {
+                                    HStack{
+                                        Text(!isShiny ? "Shiny": "Classic").foregroundColor(.black)
+                                            .font(.custom("AmericanTypewriter",fixedSize: 22)).padding(3).padding(.leading,25).padding(.trailing,10)
+                                    }.background(!isShiny ? .yellow : .white).roundedCorner(20, corners: [.topRight])
+                                }.transition(.slide)
+                                Spacer()
                                 HStack{
                                     Text("\(apiManager.pokemonList[selectedPokemonIndex-1].name.capitalized)")
-                                        
                                         .font(.custom("AmericanTypewriter",fixedSize: 22)).padding(3).padding(.leading,25).padding(.trailing,10)
                                 }.transition(.backslide).background(.white).roundedCorner(20, corners: [.bottomLeft])
+                                
                             }else{
                                 HStack{}.padding(16)
                             }
@@ -87,22 +95,29 @@ struct ContentView: View {
                             Button {
                                 isImageFront.toggle()
                             } label: {
-                                    AsyncImage(url: URL(string:  isImageFront
-                                    ? "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+String(selectedPokemonIndex)+".png"
-                                    : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/"+String(selectedPokemonIndex)+".png"))
-                                    { image in
-                                        image.resizable()
-                                    } placeholder: {
-                                        ProgressView()
-                                    }.frame(width: 250, height: 250).background(.white.opacity(0.3)).cornerRadius(180.0).transition(.scale)
+                                AsyncImage(url: URL(string:  isImageFront
+                                                    ? isShiny ?  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/"+String(selectedPokemonIndex)+".png"
+                                                    :
+                                                        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+String(selectedPokemonIndex)+".png"
+                                                    : isShiny ?  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/"+String(selectedPokemonIndex)+".png"
+                                                    : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/"+String(selectedPokemonIndex)+".png"))
+                                { image in
+                                    image.resizable()
+                                } placeholder: {
+                                    ProgressView()
+                                }.frame(width: 250, height: 250).background(.white.opacity(0.3)).cornerRadius(180.0).transition(.scale)
                             }
                             HStack{
-                                Image(types[0]).resizable().frame(width: 90,height: 35).padding(20)
+                                HStack{
+                                    Image(types[0]).resizable().frame(width: 90,height: 35).padding(20)
+                                }.transition(.backslide)
                                 Spacer()
                                 if types.count > 1{
-                                    Image(types[1]).resizable().frame(width: 90,height: 35).padding(20)
+                                    HStack{
+                                        Image(types[1]).resizable().frame(width: 90,height: 35).padding(20)
+                                    }.transition(.slide)
                                 }
-                            }.transition(.slide)
+                            }
                         }else{
                             Spacer()
                             Image("openPokeball").resizable().frame(width: 80,height: 55)
@@ -110,18 +125,18 @@ struct ContentView: View {
                         }
                         
                         Spacer()
-                            Picker("",selection:$selectedPokemonIndex){
-                                ForEach(apiManager.pokemonList){ pok in
-                                    HStack{
-                                        Image("pokeballSpin").resizable().frame(width: 30,height: 30)
-                                        Text(pok.name.capitalized).tag(pok.id)
-                                        Spacer()
-                                        Text(pok.pokemonId)
-                                    }
+                        Picker("",selection:$selectedPokemonIndex){
+                            ForEach(apiManager.pokemonList){ pok in
+                                HStack{
+                                    Image("pokeballSpin").resizable().frame(width: 30,height: 30)
+                                    Text(pok.name.capitalized).tag(pok.id)
+                                    Spacer()
+                                    Text(pok.pokemonId)
                                 }
-                            }.pickerStyle(.wheel)
-                                .onChange(of: selectedPokemonIndex) { tag in  Task{await pokemonChanged(tag)}}
-                    
+                            }
+                        }.pickerStyle(.wheel)
+                            .onChange(of: selectedPokemonIndex) { tag in  Task{await pokemonChanged(tag)}}
+                        
                     }
                 }
                 
